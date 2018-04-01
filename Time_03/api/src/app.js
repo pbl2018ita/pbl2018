8af202e2dbbf0af418a5ec17ee53c27e4ddcad29
Fabio Kfouri <kfouri.fabio@gmail.com>
2018-04-01 11:02:56 -0300

    Objetos CRUD

--- Time_03/api/src/app.js
+++ Time_03/api/src/app.js
@@ -2,13 +2,18 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const app = express(); //Cria aplicacao
 
+
+const app = express(); //Cria aplicacao
 const router = express.Router(); // criar um router
+
+//poderia configurar outas coisas pelo bodyParser, como tamanho maximo da requisicao
+app.use(bodyParser.json()); //todo conteudo ser convertido para json
+app.use(bodyParser.urlencoded({ extended: false })) //para codificar as urls
 
 //get -> obter informacoes
 const route = router.get('/', (req, res, next) => {
-    //200 -> 0
+    //200 -> OK
     //201 -> created
     //400 -> Bad request
     //401 -> nao autenticado
@@ -25,6 +30,22 @@ const create = router.post('/', (req, res, next) => {
     res.status(201).send(req.body);
 });
 
+//put -> atualizar informacoes
+const put = router.put('/:id', (req, res, next) => {
+    let id = req.params.id;
+    res.status(201).send({
+        id: id,
+        item: req.body
+    });
+});
+
+const del = router.delete('/', (req, res, next) => {
+    res.status(200).send(req.body);
+});
+
 app.use('/', route);
+app.use('/teste', create);
+app.use('/teste', put);
+app.use('/teste', del);
 
 module.exports = app;
