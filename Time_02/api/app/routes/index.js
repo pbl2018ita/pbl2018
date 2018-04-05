68f939681b7c3f0ffb76ffdbcf26d5ec4766a93e
SamaraCardoso27 <samaracardosodossantos@gmail.com>
2018-04-04 22:46:23 -0300

    #71 Trabalhando na US109b

--- Time_02/api/app/routes/index.js
+++ Time_02/api/app/routes/index.js
@@ -2,10 +2,12 @@ var express = require('express')
 
 module.exports = function (app) {
   'use strict'
-  var leitosHandlers = require('../controllers/leitos')
   var indexHandlers = require('../controllers/index')
+  var leitosHandlers = require('../controllers/leitos') //US108
+  var patientsReceiptHandlers = require('../controllers/patients_receipt') //US109b
   
   app.get('/', indexHandlers.index) 
-  app.get('/leitos', leitosHandlers.getLeitos)
-  
+  app.get('/leitos', leitosHandlers.getLeitos) //US108
+  app.post('/patients_receipt', patientsReceiptHandlers.getPatientsReceipt) //US109b
 }
+
