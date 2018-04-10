918183accc2bb0fbf2702a89bb0d9754c99e9fff
AlysonBasilio <alyson.basilio@gmail.com>
2018-04-09 21:49:02 -0300

    us109a mockada

--- Time_02/api/app/routes/index.js
+++ Time_02/api/app/routes/index.js
@@ -1,11 +1,11 @@
-var express = require('express')
+var express = require('express');
 
 module.exports = function (app) {
-  'use strict'
-  var leitosHandlers = require('../controllers/leitos')
-  var indexHandlers = require('../controllers/index')
-  
-  app.get('/', indexHandlers.index) 
-  app.get('/leitos', leitosHandlers.getLeitos)
-  
+    'use strict'
+    var leitosHandlers = require('../controllers/leitos');
+    var indexHandlers = require('../controllers/index');
+    var vitimaHandlers = require('../controllers/vitima');
+    app.get('/', indexHandlers.index);
+    app.get('/leitos', leitosHandlers.getLeitos);
+    app.get('/vitima', vitimaHandlers.getVitimas);
 }
