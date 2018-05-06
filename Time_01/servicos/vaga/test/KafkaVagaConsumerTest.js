990de240e1039039a24036fd096fee0bc0d2c084
Wilson Cristoni Neto <wcristoni@gmail.com>
2018-05-06 20:31:07 -0300

    incluindo um log no exemplo do kafka

--- Time_01/servicos/vaga/test/KafkaVagaConsumerTest.js
+++ Time_01/servicos/vaga/test/KafkaVagaConsumerTest.js
@@ -6,6 +6,8 @@ consumer = new Consumer(
     client, [ { topic: 'cross' } ], { autoCommit: false });
     //client, [ { topic: 'cross', partition: 0 } ], { autoCommit: false });
 
+    console.log("tentando conexão...");
+
 consumer.on('message', function (message) {
     //var data = JSON.parse(message.value);
     console.log(message.value + "\n\n");
