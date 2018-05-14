6d8cc7ff2347b492f707c021bd20ced15dee94c3
SamaraCardoso27 <samaracardosodossantos@gmail.com>
2018-05-14 20:42:45 -0300

    #115 Trabalhando

--- Time_02/api/app/controllers/leitos.js
+++ Time_02/api/app/controllers/leitos.js
@@ -2,9 +2,46 @@
 
 var mongoose = require('mongoose')
 var Leitos = mongoose.model('Leitos')
+var kafka = require('kafka-node')
+var Producer = kafka.Producer,
+    client = new kafka.Client('stagihobd.hashtagsource.com:2181'),
+    producer = new Producer(client);
+
+producer.on('ready', function () {
+    console.log('Producer is ready');
+});
+producer.on('error', function (err) {
+    console.log('Producer is in error state');
+    console.log(err);
+})
+
+
+var topico = "cross";
+var payloads;
+var query_leitos;
+var leitos_disponiveis;
+
+
+function getLeitos(callback) {
+  Leitos.find({}, function(err, leitos) {
+    if (err) {
+      callback(err, null);
+    } else {
+      callback(null, leitos);
+    }
+  });
+};
+
+getLeitos(function(err, leitos) {
+    if (err) {
+        console.log(err);
+    }
+   leitos_disponiveis = leitos
+});
+
+
 
 exports.getLeitos = function (req, res) {
-
   var hospital = req.query.hospital
   var status = req.query.status
   var identificador = req.query.identificador
@@ -15,13 +52,26 @@ exports.getLeitos = function (req, res) {
         "nome": hospital,
       }
   }
-      
-  Leitos.find({hospital:hospital, status:status},{setor:1, andar:1, ala:1,tipo:1}, function(err, leitos) {
-    if (err) return res.status(400).send(err)
-    if (leitos){
-      result["hospital"]["leitos"] = leitos
-      res.send(result)
-    }
-    else return res.status(400).json({ message: 'Leito não encontrado' })
-  });
+
+
+  var obj = {};
+
+  obj['leitos'] = leitos_disponiveis.filter( x => {
+      return x.hospital == req.query.hospital && x.status == req.query.status;
+  })
+   
+  try {
+    var sentMessage = JSON.stringify(obj);
+    payloads = [
+      { topic: topico, messages:sentMessage , partition: 0 }
+    ];
+    producer.send(payloads, function (err, data) {
+      console.log('Resultado adicionado no kafka');
+    });
+  }catch (err) {
+    console.error(err);
+    res.status(500).send({result: "fail"});
+  }
+  
+  res.send(obj);
 }
