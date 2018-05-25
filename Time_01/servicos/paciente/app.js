35135714ab5e24e49140e062729deeba19276266
Wilson Cristoni Neto <wcristoni@gmail.com>
2018-05-25 20:20:24 -0300

    Ajustando a V1 do microserviço Interface FACEDE, para integração com o Blockchain  (US-300b), incluindo a PROCEDURE dinamica e um mock com retornos dinâmicos para simular o cadastro no BlockChain, incluindo também os scripts de banco de dados

--- Time_01/servicos/paciente/app.js
+++ Time_01/servicos/paciente/app.js
@@ -21,7 +21,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/api', pacienteRouter);
-
+console.log('Serviço PACIENTE iniciado.');
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
--- Time_01/servicos/paciente/routes/paciente.js
+++ Time_01/servicos/paciente/routes/paciente.js
@@ -24,19 +24,31 @@ function validarRetorno(hashBlochChain, res){
 function cadastrarPaciente(callback, obj, res){
 
   console.log("TODO: Implementar o cadastro paciente no BlockChain");
-
   // TODO: Implementar a comunicação com o Serviço do BlockChain, quando estiver pronto
-  var hashBlochChain = "46070d4bf934fb0d4b06d9e2c46e346944e322444900a435d7d9a95e6d7435f5"
-  validarRetorno(hashBlochChain, res)
+  
+  var hashBlochChainMock = [
+    "BD5943F620B00FDCB67088D884313CFCF5AFEDC336B97E88BB1EAD9AE748DD99",
+    "0A2EE86E3FD11F54115957CCD03B9E4D2009409717449C10EA138A549D590D60",
+    "696BB81019567598AFE3AE7EEF75660F292BE08AC39739E96C4FAB3E4367B064",
+    "92F30ED626D3256CDEC0F47F0B6F23B661F2224A50FE42140EE6BF1FED3FD56A",
+    "51F1EF0D77133098A9E6524A8F6323CA4948C9199ED25C557F2A45A8EAC99BBB",
+    "203006FB3B0B26126A45C2647A3BD6ECD4F11BD32C96ECC76742E58AD4F6D153",
+    "3087CF1A0238407088D1D20170AFA8E19487EAE4375BB5479236C87F3E31E71D",
+    "71958E05D79E8F1EB28F93F25D341200A81365485EAEE09E2F1E0ED9D6277FDE",
+    "BD5943F620B00FDCB67088D884313CFCF5AFEDC336B97E88BB1EAD9AE748DD99",
+    "4850C415D7F66EB4434CC0288638E2823D9677BB82B683D77712E1A829D51503"
+  ];
+
+  validarRetorno(hashBlochChainMock[Math.random() * (9 - 0)], res)
 }
 
 function BuscarHash(callback, campo, valor, obj, res){
 
-  var querySQL = 'SELECT hashBC AS hashBC FROM PACIENTE WHERE ' + campo + '= "' + valor + '" ;';
+  // var querySQL = 'SELECT hashBC AS hashBC FROM PACIENTE WHERE ' + campo + '= "' + valor + '" ;';
+
+  var querySQL = 'call sp_LocalizarPaciente("' + campo + '", "' + valor + '");';
   var hashBlochChain;
   
-  console.log(querySQL);
-
   //Conectar no banco de dados
   connection = mysql.createConnection({
     host     : 'stagihobd.hashtagsource.com',
@@ -52,14 +64,15 @@ function BuscarHash(callback, campo, valor, obj, res){
     }
     try{
       if (rows){
-        hashBlochChain = rows[0].hashBC;
-        console.log('O Hash do paciente é: ', hashBlochChain);
+        hashBlochChain = (rows[0][0].hashBC) ? rows[0][0].hashBC: rows[0].hashBC;
+         // rows[0][0].hashBC -> é o retornoo se for utiizada PROCEDURE
+         // rows[0].hashBC    -> se for utiizada SELECT hard code
       }
     }
     catch(erro){
       hashBlochChain = null;
     }
-    
+    console.log('O Hash do paciente é: ', hashBlochChain);
   });
   connection.end();
 
