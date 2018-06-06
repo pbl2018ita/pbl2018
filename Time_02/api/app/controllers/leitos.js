cf9f7b03dd88bbbda7f706e626c79804e48f34d0
AlysonBasilio <alyson.basilio@gmail.com>
2018-06-05 22:44:32 -0300

    reserva de leito feita

--- Time_02/api/app/controllers/leitos.js
+++ Time_02/api/app/controllers/leitos.js
@@ -7,10 +7,10 @@ var Producer = kafka.Producer,
     client = new kafka.Client('stagihobd.hashtagsource.com:2181'),
     producer = new Producer(client);
 
-producer.on('ready', function () {
+producer.on('ready', function() {
     console.log('Producer is ready');
 });
-producer.on('error', function (err) {
+producer.on('error', function(err) {
     console.log('Producer is in error state');
     console.log(err);
 })
@@ -23,55 +23,59 @@ var leitos_disponiveis;
 
 
 function getLeitos(callback) {
-  Leitos.find({}, function(err, leitos) {
-    if (err) {
-      callback(err, null);
-    } else {
-      callback(null, leitos);
-    }
-  });
+    Leitos.find({}, function(err, leitos) {
+        if (err) {
+            callback(err, null);
+        } else {
+            callback(null, leitos);
+        }
+    });
 };
 
 getLeitos(function(err, leitos) {
     if (err) {
         console.log(err);
     }
-   leitos_disponiveis = leitos
+    leitos_disponiveis = leitos
 });
 
 
 
-exports.getLeitos = function (req, res) {
-  var hospital = req.query.hospital
-  var status = req.query.status
-  var identificador = req.query.identificador
+exports.getLeitos = function(req, res) {
+    var hospital = req.query.hospital
+    var status = req.query.status
+    var identificador = req.query.identificador
 
-  var result = {
-      "hospital": {
-        "identificador": identificador,
-        "nome": hospital,
-      }
-  }
+    var result = {
+        "hospital": {
+            "identificador": identificador,
+            "nome": hospital,
+        }
+    }
 
 
-  var obj = {};
+    var obj = {};
 
-  obj['leitos'] = leitos_disponiveis.filter( x => {
-      return x.hospital == req.query.hospital && x.status == req.query.status;
-  })
-   
-  try {
-    var sentMessage = JSON.stringify(obj);
-    payloads = [
-      { topic: topico, messages:sentMessage , partition: 0 }
-    ];
-    producer.send(payloads, function (err, data) {
-      console.log('Resultado adicionado no kafka');
-    });
-  }catch (err) {
-    console.error(err);
-    res.status(500).send({result: "fail"});
-  }
-  
-  res.send(obj);
+    obj['leitos'] = leitos_disponiveis.filter(x => {
+        return x.hospital == req.query.hospital && x.status == req.query.status;
+    })
+
+    try {
+        var sentMessage = JSON.stringify(obj);
+        payloads = [{
+            topic: topico,
+            messages: sentMessage,
+            partition: 0
+        }];
+        producer.send(payloads, function(err, data) {
+            console.log('Resultado adicionado no kafka');
+        });
+    } catch (err) {
+        console.error(err);
+        res.status(500).send({
+            result: "fail"
+        });
+    }
+
+    res.send(obj);
 }
--- Time_02/api/app/controllers/reserva.js
+++ Time_02/api/app/controllers/reserva.js
@@ -13,10 +13,10 @@ var Producer = kafka.Producer,
     client = new kafka.Client('stagihobd.hashtagsource.com:2181'),
     producer = new Producer(client);
 
-producer.on('ready', function () {
+producer.on('ready', function() {
     console.log('Producer is ready');
 });
-producer.on('error', function (err) {
+producer.on('error', function(err) {
     console.log('Producer is in error state');
     console.log(err);
 })
@@ -25,118 +25,130 @@ producer.on('error', function (err) {
 var kafka = require('kafka-node'),
     Consumer = kafka.Consumer,
     client = new kafka.Client('stagihobd.hashtagsource.com:2181'),
-    consumer = new Consumer(client,
-        [{ topic: topico, offset: 0}],
-        {
-            autoCommit: false
-        }
-    );
+    consumer = new Consumer(client, [{
+        topic: topico,
+        offset: 0
+    }], {
+        autoCommit: false
+    });
 
 
-consumer.on('message', function (message) {
-  mensagem = message
-  console.log(message); 
+consumer.on('message', function(message) {
+    mensagem = message
+    console.log(message);
 });
 
-consumer.on('error', function (err) {
-    console.log('Error:',err);
+consumer.on('error', function(err) {
+    console.log('Error:', err);
 })
 
 
 
-exports.newReserva = function (req, res) {
-  // Add no kafka
-  try {
-    var sentMessage = JSON.stringify(req.body);
-    payloads = [
-      { topic: topico, messages:sentMessage , partition: 0 }
-    ];
-    producer.send(payloads, function (err, data) {
-      res.json(data);
-    });
-  }catch (err) {
-    console.error(err);
-    res.status(500).send({result: "fail"});
-  }
-
-  // Add mongodb
-  var reserva_post = new Reserva ({
-      id_paciente:     req.body.id_paciente,
-      id_leito:        req.body.id_leito,
-      id_plantonista:  req.body.id_plantonista,
-      id_especialista: req.body.id_especialista,
-      data_internacao: req.body.data_internacao
-  });
-  reserva_post.save(function (err, data) {    	
-    if (err) return res.status(400).send(err)
-    console.log('Nova reserva realizada com sucesso') 	
-  });
-}
-
-
-exports.getReservas = function (req, res) {
-  Reserva.find({}, function(err, reservas) {
-    if (err) {
-      console.error(err);
-    } else {
-      res.send(reservas)
+exports.newReserva = function(req, res) {
+    // Add no kafka
+    try {
+        var sentMessage = JSON.stringify(req.body);
+        payloads = [{
+            topic: topico,
+            messages: sentMessage,
+            partition: 0
+        }];
+        producer.send(payloads, function(err, data) {
+            return;
+        });
+    } catch (err) {
+        console.error(err);
+        res.status(500).send({
+            result: "fail"
+        });
     }
-  });
+
+    // Add mongodb
+    var reserva_post = new Reserva({
+        id_paciente: req.body.id_paciente,
+        id_leito: req.body.id_leito,
+        id_plantonista: req.body.id_plantonista,
+        id_especialista: req.body.id_especialista,
+        data_internacao: req.body.data_internacao
+    });
+    reserva_post.save(function(err, data) {
+        if (err) return res.status(400).send(err);
+        res.redirect('/view/reserva');
+    });
 }
 
-exports.updateReserva = function (req, res) {
-    var id_reserva      = req.body.id_reserva;
-    var id_leito        = req.body.id_leito;
-    var id_paciente     = req.body.id_paciente;
+
+exports.getReservas = function(req, res) {
+    Reserva.find({}, function(err, reservas) {
+        if (err) {
+            console.error(err);
+        } else {
+            res.send(reservas)
+        }
+    });
+}
+
+exports.updateReserva = function(req, res) {
+    var id_reserva = req.body.id_reserva;
+    var id_leito = req.body.id_leito;
+    var id_paciente = req.body.id_paciente;
     var data_internacao = req.body.data_internacao;
-    var id_plantonista  = req.body.id_plantonista;
+    var id_plantonista = req.body.id_plantonista;
     var id_especialista = req.body.id_especialista;
 
-    Reserva.findById({"_id": id_reserva}, function(err, result) {
-     if (err) return res.status(400).send(err)
-     if (result){
-            result.id_paciente =  id_paciente,
-            result.id_leito =  id_leito,
-            result.data_internacao =  data_internacao
+    Reserva.findById({
+        "_id": id_reserva
+    }, function(err, result) {
+        if (err) return res.status(400).send(err)
+        if (result) {
+            result.id_paciente = id_paciente,
+                result.id_leito = id_leito,
+                result.data_internacao = data_internacao
             result.id_plantonista = req.body.id_plantonista,
-            result.id_especialista = req.body.id_especialista,
+                result.id_especialista = req.body.id_especialista,
 
-        result.save(function(err) {
-            if (err) return res.status(400).send(err)
-            else res.send("Reserva atualizada com sucesso")
-       });
-     }
-     else return res.status(400).json({ message: 'Reserva não encontrada' })
-   });
+                result.save(function(err) {
+                    if (err) return res.status(400).send(err)
+                    else res.send("Reserva atualizada com sucesso")
+                });
+        } else return res.status(400).json({
+            message: 'Reserva não encontrada'
+        })
+    });
 }
 
 
-exports.deleteReserva = function (req, res) {
-  Reserva.remove ( {"_id": req.body.id_reserva}, function (err, data) {    	
-    if (err) return res.status(400).send(err)    	
-      else res.send("Remover reserva, realizada com sucesso")    
+exports.deleteReserva = function(req, res) {
+    Reserva.remove({
+        "_id": req.body.id_reserva
+    }, function(err, data) {
+        if (err) return res.status(400).send(err)
+        else res.send("Remover reserva, realizada com sucesso")
     });
 
 }
 
 
-exports.confReserva = function (req, res) {
-    var id_reserva      = req.body.id_reserva;
-    var id_leito        = req.body.id_leito;
-    var id_paciente     = req.body.id_paciente;
+exports.confReserva = function(req, res) {
+    var id_reserva = req.body.id_reserva;
+    var id_leito = req.body.id_leito;
+    var id_paciente = req.body.id_paciente;
     var data_internacao = req.body.data_internacao;
-    var id_plantonista  = req.body.id_plantonista;
+    var id_plantonista = req.body.id_plantonista;
     var id_especialista = req.body.id_especialista;
 
-    Reserva.findById({"_id": id_reserva}, function(err, result) {
-     if (err) return res.status(400).send(err)
-     if (result){
-            result.status =  "Ocupado",
-        result.save(function(err) {
-            if (err) return res.status(400).send(err)
-            else res.send("Reserva atualizada com sucesso")
-       });
-     }
-     else return res.status(400).json({ message: 'Reserva não encontrada' })
-   });
+    Reserva.findById({
+        "_id": id_reserva
+    }, function(err, result) {
+        if (err) return res.status(400).send(err)
+        if (result) {
+            result.status = "Ocupado",
+                result.save(function(err) {
+                    if (err) return res.status(400).send(err)
+                    else res.send("Reserva atualizada com sucesso")
+                });
+        } else return res.status(400).json({
+            message: 'Reserva não encontrada'
+        })
+    });
 }
