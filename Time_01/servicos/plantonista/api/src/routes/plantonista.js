a91dce14ca6ff7107c53d4f9ba2813e1988fedb5
Jean Carlos <jeancarlosfatec@gmail.com>
2018-06-12 00:20:12 -0300

    Integração com Kafka
    Integração com Kafka

--- Time_01/servicos/plantonista/api/src/routes/plantonista.js
+++ Time_01/servicos/plantonista/api/src/routes/plantonista.js
@@ -4,10 +4,13 @@ const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/plantonista-controller');
 
+
+
 router.get('/',controller.get);
 router.get('/:codigo',controller.getById);
 router.post('/',controller.post);
 router.put('/:codigo',controller.put);
 router.delete('/del/:codigo', controller.del);
+
 
 module.exports = router;
