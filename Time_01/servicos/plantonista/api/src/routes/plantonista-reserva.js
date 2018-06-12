49452cfe2d7d486bd2702da35e1c5842302d3db6
Jean Carlos <jeancarlosfatec@gmail.com>
2018-06-12 00:19:31 -0300

    Integração com Kafka
    Integração com Kafka

--- Time_01/servicos/plantonista/api/src/routes/plantonista-reserva.js
+++ Time_01/servicos/plantonista/api/src/routes/plantonista-reserva.js
@@ -6,14 +6,12 @@ const controller = require('../controllers/plantonista-reserva-controller');
 
 // Reserva
 
-
 console.log("routes-plantonista-reserva: " + controller.post);
 
 router.get('/',controller.get);
 router.get('/:codigo',controller.getById);
+router.get('/plantonista-reserva-status',controller.get);
 router.post('/',controller.post);
 router.put('/confirmar/:crm',controller.put);
-//router.put('/cancelar',controller.put);
-//router.put('/finalizar',controller.put);
 
 module.exports = router;
